c255ef51257f20f15f230781cfa89f3952487229
nanna-dk <nanna@e-nanna.dk>
2023-07-05 15:14:28 +0200

    Setup

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -27,23 +27,23 @@ call_user_func(function ($extKey ='ucph_page_color_themes') {
                         ],
                         [
                             'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:color_1',
-                            'color_1',
+                            'subset_color_1',
                         ],
                         [
                             'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:color_2',
-                            'color_2',
+                            'subset_color_2',
                         ],
                         [
                             'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:color_3',
-                            'color_3',
+                            'subset_color_3',
                         ],
                         [
                             'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:color_4',
-                            'color_4',
+                            'subset_color_4',
                         ],
                         [
                             'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:color_5',
-                            'color_5',
+                            'subset_color_5',
                         ],
                     ],
                 ],
